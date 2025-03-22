b3485ef81ff9208a6f2d28fe2d3ea6df094744e5
Joseth Salinas <josethsg06@gmail.com>
2025-03-21 23:11:29 -0300

    Update calculator

---
+++ Calculator.js
@@ -0,0 +1,76 @@
+// variables de la calculadora
+let numero1 = 
+
+//Funciones de la calculadora
+function porcentaje(){
+
+}
+function borrado_Imput_Full(){
+
+}
+function borrado_imput(){
+
+}
+function borrarNumero(){
+
+}   
+function numero1eX(){
+
+}
+function numeroXcuadrado(){
+
+}
+function numeroRaiz(){
+
+}
+function divisor(){
+
+}
+function numeroSiete(){
+
+}
+function numeroOcho(){
+
+}
+function numeroNueve(){
+
+}
+function multiplicador(){
+
+}
+function numeroCuatro(){
+
+}
+function numeroCinco(){
+
+}
+function numeroSeis(){
+
+}
+function numeromenos(){
+
+}
+function numeroUno(){
+
+}
+function numeroDos(){
+
+}
+function numeroTres(){
+
+}
+function suma(){
+
+}
+function CambioDeSigno(){
+
+}
+function numeroCero(){
+
+}
+function coma() {
+
+}
+function igual(){
+
+}
